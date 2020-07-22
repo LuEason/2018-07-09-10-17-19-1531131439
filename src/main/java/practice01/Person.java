5d6ed6e42fffdd2df2e0f006fd46dc5dab106efe
Eason <chenzscd@163.com>
2020-07-22 20:22:19 +0800

    [OO Step by Step: practice01] [Eason] [feature & test]

--- src/main/java/practice01/Person.java
+++ src/main/java/practice01/Person.java
@@ -1,4 +1,34 @@
 package practice01;
 
+import java.util.Objects;
+
 public class Person {
+    protected String name;
+
+    protected Integer age;
+
+    public Person(String name, Integer age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public String introduce() {
+        return "My name is " + name + ". I am " + age + " years old.";
+    }
 }
