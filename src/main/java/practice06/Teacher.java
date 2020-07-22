6a3f4df2ab81cf80751088574ac9b59de295154a
Eason <chenzscd@163.com>
2020-07-22 20:24:07 +0800

    [OO Step by Step: practice06] [Eason] [feature & test]

--- src/main/java/practice06/Teacher.java
+++ src/main/java/practice06/Teacher.java
@@ -1,4 +1,31 @@
 package practice06;
 
-public class Teacher {
+public class Teacher extends Person {
+    private Integer klass;
+
+    public Teacher(String name, Integer age) {
+        super(name, age);
+    }
+
+    public Teacher(String name, Integer age, Integer klass) {
+        super(name, age);
+        this.klass = klass;
+    }
+
+    public Integer getKlass() {
+        return klass;
+    }
+
+    public void setKlass(Integer klass) {
+        this.klass = klass;
+    }
+
+    @Override
+    public String introduce() {
+        if (klass != null) {
+            return super.introduce() + " I am a Teacher. I teach Class " + klass + ".";
+        } else {
+            return super.introduce() + " I am a Teacher. I teach No Class.";
+        }
+    }
 }
